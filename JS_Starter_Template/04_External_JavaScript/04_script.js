d3a08a4f0859417c3d45147d9db5afcf5cc46827
Aadeshkale <aadeshkale0@gmail.com>
2018-07-30 18:44:35 +0530

    Js-Devlopement started

---
+++ JS_Starter_Template/04_External_JavaScript/04_script.js
@@ -0,0 +1,14 @@
+// Add your JavaScript code here
+
+function blue() {
+    document.querySelector('#green_p').style.backgroundColor='blue';
+}
+function red() {
+    document.querySelector('#green_p').style.backgroundColor='red';
+}x
+function facebook() {
+    onclick="document.querySelector('#myImage').setAttribute('src','../img/youtube.jpg')";
+}
+function youtube() {
+    onclick="document.querySelector('#myImage').setAttribute('src','../img/youtube.jpg')";
+}
